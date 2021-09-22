8a3ed3e33c7a53a516eee14b75709be988af6504
Artur Opala <250927+arturopala@users.noreply.github.com>
2021-09-22 09:46:15 +0100

    update the template

--- src/main/g8/it/$packaged$/controllers/$servicePrefix$JourneyISpec.scala
+++ src/main/g8/it/$packaged$/controllers/$servicePrefix$JourneyISpec.scala
@@ -1,18 +1,26 @@
 package $package$.controllers
 
+import akka.actor.ActorSystem
 import play.api.libs.json.Format
+import play.api.libs.ws.DefaultWSCookie
+import play.api.libs.ws.StandaloneWSRequest
+import play.api.mvc.AnyContent
+import play.api.mvc.Call
+import play.api.mvc.Cookie
+import play.api.mvc.Request
 import play.api.mvc.Session
-import uk.gov.hmrc.crypto.{ApplicationCrypto, PlainText}
+import play.api.test.FakeRequest
+import uk.gov.hmrc.crypto.ApplicationCrypto
+import uk.gov.hmrc.crypto.PlainText
 import $package$.journeys.$servicePrefix$JourneyStateFormats
-import $package$.models._
-import $package$.services.{MongoDBCachedJourneyService, $servicePrefix$JourneyService}
-import $package$.support.{ServerISpec, TestJourneyService}
+import $package$.repository.CacheRepository
+import $package$.services.MongoDBCachedJourneyService
+import $package$.services.$servicePrefix$JourneyService
+import $package$.support.ServerISpec
+import $package$.support.TestJourneyService
 
 import java.time.LocalDate
 import scala.concurrent.ExecutionContext.Implicits.global
-import play.api.libs.ws.DefaultWSCookie
-import akka.actor.ActorSystem
-import $package$.repository.CacheRepository
 class $servicePrefix$JourneyISpec extends $servicePrefix$JourneyISpecSetup {
 
   import journey.model.State._
@@ -51,13 +59,46 @@ trait $servicePrefix$JourneyISpecSetup extends ServerISpec {
     override def getJourneyId(journeyId: JourneyId): Option[String] = Some(journeyId.value)
   }
 
-  final def request(path: String)(implicit journeyId: JourneyId) = {
-    val sessionCookie = sessionCookieBaker.encodeAsCookie(Session(Map(journey.journeyKey -> journeyId.value)))
+  final def fakeRequest(cookies: Cookie*)(implicit
+    journeyId: JourneyId
+  ): Request[AnyContent] =
+    fakeRequest("GET", "/", cookies: _*)
+
+  final def fakeRequest(method: String, path: String, cookies: Cookie*)(implicit
+    journeyId: JourneyId
+  ): Request[AnyContent] =
+    FakeRequest(Call(method, path))
+      .withCookies(cookies: _*)
+      .withSession(journey.journeyKey -> journeyId.value)
+
+  final def request(path: String)(implicit journeyId: JourneyId): StandaloneWSRequest = {
+    val sessionCookie =
+      sessionCookieBaker
+        .encodeAsCookie(Session(Map(journey.journeyKey -> journeyId.value)))
+    wsClient
+      .url(s"\$baseUrl\$path")
+      .withCookies(
+        DefaultWSCookie(
+          sessionCookie.name,
+          sessionCookieCrypto.crypto.encrypt(PlainText(sessionCookie.value)).value
+        )
+      )
+  }
+
+  final def requestWithCookies(path: String, cookies: (String, String)*)(implicit
+    journeyId: JourneyId
+  ): StandaloneWSRequest = {
+    val sessionCookie =
+      sessionCookieBaker
+        .encodeAsCookie(Session(Map(journey.journeyKey -> journeyId.value)))
 
     wsClient
       .url(s"\$baseUrl\$path")
       .withCookies(
-        DefaultWSCookie(sessionCookie.name, sessionCookieCrypto.crypto.encrypt(PlainText(sessionCookie.value)).value)
+        (cookies.map(c => DefaultWSCookie(c._1, c._2)) :+ DefaultWSCookie(
+          sessionCookie.name,
+          sessionCookieCrypto.crypto.encrypt(PlainText(sessionCookie.value)).value
+        )): _*
       )
   }
 }
